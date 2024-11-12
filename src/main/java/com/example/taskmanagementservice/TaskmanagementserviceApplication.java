c4c9c094929f2cec8a1fbf6d2c2f767590d3a10c
Moulali Mada <moulalimoula190@gmail.com>
2024-11-12 18:12:48 +0530

    initial code

---
+++ src/main/java/com/example/taskmanagementservice/TaskmanagementserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.taskmanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmanagementserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskmanagementserviceApplication.class, args);
+	}
+
+}
